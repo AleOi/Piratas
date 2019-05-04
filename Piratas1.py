599fee1b363f252e8dcc7f456dfa740c59aec52a
AleOi <alexandre.jtsubaki@gmail.com>
2019-05-04 07:13:37 -0300

    Codigo Teste

---
+++ Piratas1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  4 07:11:01 2019
+
+@author: ale
+"""
+
+import pdb
+import numpy as np
+'''
+    Código utilizado na aula de 
+    Criação de novos piratas
+
+'''
+
+print("Recrutamento de novos piratas")
